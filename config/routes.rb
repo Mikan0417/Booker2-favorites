796cfde2dbcc9b37a7fc42a7789cac59d6f38510
EC2 Default User <ec2-user@ip-172-31-25-192.ec2.internal>
2020-09-17 20:45:00 +0000

    [Bookers] 9.18.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
-  get 'home/about' => 'home#about'
+  root to: 'homes#index'
+  get 'home/about' => 'homes#about'
   
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
